ab0411961f0b27d79c0cf0fa30c64b37c1ac1435
ChristoRutter <rutterchris54@gmail.com>
2022-02-20 14:05:34 -0500

    CSS to home page

--- client/src/components/ItemList.js
+++ client/src/components/ItemList.js
@@ -68,38 +68,97 @@ const ItemList = (props) => {
 
 
     return (
-        <div style={{ textAlign: "center" }}>
-            <header>
-                <h1>The Collection</h1>
-                <Link to={"/new"}>Add a New Item to the Collection Page</Link>
-                <button onClick={logout}>Logout</button>
+        // <div style={{ textAlign: "center" }}>
+        //     <header>
+        //         <h1>The Collection</h1>
+        //         <Link to={"/new"}>Add a New Item to the Collection Page</Link>
+        //         <button onClick={logout}>Logout</button>
 
-            </header>
+        //     </header>
 
-            {
+        //     {
+        //         list.map((item, index) => (
+        //             <div key={index}>
+
+                        
+                        
+        //                 <Link to={`/item/${item._id}`}>
+        //                     <p>{item.name}</p>
+        //                     <img src={item.image} alt="picture"
+                                
+                                
+        //                         />
+        //                 </Link>
+
+
+        //                 <div>
+        //                     <Link to={`/item/edit/${item._id}`}>Edit</Link>
+        //                     <button onClick={() => deleteItem(item._id)}>
+        //                         Delete
+        //                     </button>
+        //                 </div>
+
+        //             </div>
+        //         ))
+        //     }
+        // </div>
+
+        <section class="text-gray-600 body-font">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-wrap w-full mb-8">
+      <div class="w-full mb-6 lg:mb-0">
+      <Link to={"/new"}>Add a New Item to the Collection Page</Link>
+        <button class="mx-12 my-12" onClick={logout}>Logout</button>
+        <h1 class="sm:text-4xl text-5xl font-bold font-medium title-font mb-2 text-gray-900">The Collection</h1>
+        <div class="h-1 w-200 bg-indigo-500 rounded content-center"></div>
+      </div>
+    </div>
+
+
+
+        {
                 list.map((item, index) => (
-                    <div key={index}>
+                    
 
                         
                         
-                        <Link to={`/item/${item._id}`}>
-                            <p>{item.name}</p>
-                            <img src={item.image} alt="picture"
-                                style={{ width: "150px", height: "150px" }} />
-                        </Link>
+                                                   
+                            <Link key={index} to={`/item/${item._id}`} class="flex flex-wrap -m-4 justify-center">
+                                  <div class="lg:w-1/4 p-4 w-1/2">
+                                    <a class="block relative h-48 rounded overflow-hidden">
+                                      <img alt="picture" class="object-cover object-center w-full h-full block" src={item.image} />
+                                    </a>
+                                    <div class="mt-4">
+                                      
+                                      <h2 class="text-gray-900 title-font text-lg font-medium">{item.name}</h2>
+                                      <div>
+                                            <Link to={`/item/edit/${item._id}`}>Edit</Link>
+                                            <button class="mx-2 my-2 mb-12" onClick={() => deleteItem(item._id)}>
+                                                Delete
+                                            </button>
+                                      </div>
+                                    </div>
+                                  </div>
+                            </Link>
 
 
-                        <div>
-                            <Link to={`/item/edit/${item._id}`}>Edit</Link>
-                            <button onClick={() => deleteItem(item._id)}>
-                                Delete
-                            </button>
-                        </div>
 
-                    </div>
+
+
+                        
+
+
+
+                    
                 ))
             }
-        </div>
+
+   
+  </div>
+</section>
+
+            
+
     )
 }
 
