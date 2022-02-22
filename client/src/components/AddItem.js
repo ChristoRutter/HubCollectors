58e34d647fe2b200b788ce36e7fc88c2d2717bbd
ChristoRutter <rutterchris54@gmail.com>
2022-02-22 15:52:39 -0500

    File did not work

--- client/src/components/AddItem.js
+++ client/src/components/AddItem.js
@@ -71,7 +71,7 @@ const AddItem = (props) =>{
                     <input 
                         class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Image"
 
-                        value={image} onChange={(e)=>setImage(e.target.value)} type="file" />
+                        value={image} onChange={(e)=>setImage(e.target.value)} type="text" />
 
                     
                     {
